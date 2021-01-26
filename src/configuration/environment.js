0eeec3a39110b7a47b5acb9167e36ad8c8ed8e24
Neil Witten <nwitten@gmail.com>
2021-01-26 13:52:26 +0000

    More env var tests

--- src/configuration/environment.js
+++ src/configuration/environment.js
@@ -2,6 +2,8 @@ export const REQUIRED_VARIABLES = ['GOOGLE_ANALYTICS_ID', 'AIRTABLE_API_KEY'];
 
 /* eslint-disable */
 console.log(process.env);
+console.log(process.env.AIRTABLE_API_KEY)
+console.log(process.env.ANOTHER_KEY)
 console.log('env ' + process.env.REACT_APP_KEY);
 
 export const PROCESS_ENV =
